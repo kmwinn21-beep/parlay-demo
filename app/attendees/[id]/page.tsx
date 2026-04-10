8b50f155d2bc457f9bd9c25089c79b56d2438968
kmwinn21-beep <k.m.winn21@gmail.com>
2026-04-10 04:03:46 -0600

    Update page.tsx

--- app/attendees/[id]/page.tsx
+++ app/attendees/[id]/page.tsx
@@ -588,7 +588,7 @@ export default function AttendeeDetailPage() {
                 <svg className="w-5 h-5 text-procare-dark-blue" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
                 </svg>
-                <span className="text-sm font-medium text-procare-dark-blue">Assign Follow Up</span>
+                <span className="text-sm font-medium text-procare-bright-blue">Add Follow Up</span>
               </button>
             </div>
             <FollowUpsTable followUps={followUps} onToggle={handleToggleFollowUp} onDelete={handleDeleteFollowUp} userOptions={userOptions} onRepChange={handleRepChange} />
