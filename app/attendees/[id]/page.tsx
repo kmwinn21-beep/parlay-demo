e0f60e7201f165158359bb7ab6244c15896e4050
kmwinn21-beep <k.m.winn21@gmail.com>
2026-04-13 15:21:07 -0600

    Shorten button text from 'Schedule Meeting' to 'Schedule'

--- app/attendees/[id]/page.tsx
+++ app/attendees/[id]/page.tsx
@@ -668,7 +668,7 @@ export default function AttendeeDetailPage() {
                 <svg className="w-5 h-5 text-procare-dark-blue" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                 </svg>
-                <span className="text-sm font-medium text-procare-dark-blue">Schedule Meeting</span>
+                <span className="text-sm font-medium text-procare-dark-blue">Schedule</span>
               </button>
             </div>
             <MeetingsTable meetings={meetings} actionOptions={actionOptions.map(o => o.value)} colorMap={colorMaps.action || {}} userOptions={userOptions} hideCompany onOutcomeChange={handleMeetingOutcome} onDelete={handleDeleteMeeting} onEdit={async (meetingId, data) => {
